ad2d14ffa4abc448615947826b4a90153a938e34
tubhyam14 <77627781+tubhyam14@users.noreply.github.com>
2021-06-28 15:32:26 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -1,32 +1,31 @@
 class Rubber{
-	constructor(x,y,r)
-	{
-	// assign options to the rubber ball
+  constructor(x,y,r){
+	  
 var options={
-		restitution: 0.3,
-		friction:5,
-		density:1
-	}
-		this.x=x;
-		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y,this.r, options)
-		World.add(world, this.body);
+  restitution: 0.3,
+  friction:5,
+  density:1 };
+	
+ this.x=x;
+ this.y=y;
+ this.r=r
+ this.body=Bodies.circle(this.x, this.y,this.r, options);
+ World.add(world, this.body);
 
-	}
-	display(color)
-	{
-			var rubberpos=this.body.position;
+  }	
+ display(){
+	
+var rubberpos=this.body.position;
 
-			push()
-			translate(rubberpos.x, rubberpos.y);
-			strokeWeight(4);
-			stroke("black");
-			fill(color);
-			//draw the ellipse here to display the rubber ball
-			ellipseMode (RADIUS)
+push()
+translate(rubberpos.x, rubberpos.y);
+strokeWeight(4);
+stroke("black");
+fill(color);
+			
+ellipseMode (RADIUS)
 ellipse(0,0,this.r)
-			pop()
+pop()
 	}
 
 }
